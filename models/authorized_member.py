c462ef8f238491c8dad76884a67c02875d043136
joaocrespin <joaorato58@gmail.com>
2025-06-15 01:02:45 -0300

    feat: adiciona o campo 'cargo' no model e implementa redirecionamento para cadastro de autorizados

--- models/authorized_member.py
+++ models/authorized_member.py
@@ -5,4 +5,5 @@ class Authorized(db.Model):
     authorized_id = db.Column(db.Integer, primary_key=True)
     authorized_name = db.Column(db.String(255))
     cpf = db.Column(db.String(14), unique=True)
+    position = db.Column(db.String(12))
     photo = db.Column(db.Text, unique=True)
