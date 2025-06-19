a109d0f33f1d7d3bbe7b79be896d4f914456312c
joaocrespin <joaorato58@gmail.com>
2025-06-19 19:55:28 -0300

    feat: adiciona rota GET para retornar usuários cadastrados

--- routes/main.py
+++ routes/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from helpers import login_required
+from models.user import User
 
 main = Blueprint("main", __name__)
 
@@ -12,3 +13,22 @@ def index():
 @login_required
 def historico():
     return render_template("historico.html")
+
+@main.route("/usuarios")
+@login_required
+def get_users():
+    users = User.query.all()
+
+    if not users:
+        return "Nenhum membro autorizado encontrado!", 404
+
+    user_list = []
+    for user in users:
+        user_list.append({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "access_level": user.access_level,
+        })            
+    
+    return user_list
