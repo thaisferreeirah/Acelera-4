6e099bac6013f6bd15dae9a2df064ca5bd7adf05
thais <thais.ferreira1795@gmail.com>
2025-06-20 19:34:17 -0300

    atualização

--- models/authorized_member.py
+++ models/authorized_member.py
@@ -1,15 +1,3 @@
-# from db import db
-
-# class Authorized(db.Model):
-#     __tablename__ = 'authorized'
-#     authorized_id = db.Column(db.Integer, primary_key=True)
-#     authorized_name = db.Column(db.String(255))
-#     cpf = db.Column(db.String(14), unique=True)
-#     position = db.Column(db.String(12))
-#     # photo = db.Column(db.Text, unique=True)
-
-
-# models/authorized.py
 from db import db  # Mantenha como está se db está definido assim
 
 class Authorized(db.Model):
